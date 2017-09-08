78c4f9e916ef52ba9bda3dd442cfda7e58f8fd05
Elioth1 <31458423+Elioth1@users.noreply.github.com>
2017-09-07 20:38:39 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,44 @@
 ﻿using System;
-
-namespace mario
+namespace programa
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Altura de la piramide: ");
-            int niveles;
-            /// Tryparse convierte la representación de una cadena (string) a su equivalente entero (int). Un valor devuelto indica si la conversión tuvo éxito.
-            //  devuelve 0 si no se pudo convertir
-            int.TryParse( Console.ReadLine(), out niveles);
+       int altura, actual, esp, hash;
 
-             dibujarPiramide(niveles);
+       Console.WriteLine("programadores: MENDOZA MERCADO CRISTINA CECILIA Y DELGADO VALENCIA BLANCA GUADALUPE");
+       Console.WriteLine("dibujar piramide");
+       Console.WriteLine("ingresa la altura de la piramide no mayor de 23 ni numero negativo");
+       altura = Convert.ToInt32(Console.ReadLine());
+       while( altura > 23 || altura < 1)
+       {
+           Console.Write("Altura: ");
+           int.TryParse(Console.ReadLine(),out altura);
+       }
+     
+   for (esp = 0; esp < altura - actual; esp++)
+       {
+            for (hash =0; hash < (actual*2)-1; hash++)
+           {
+           Console.Write(" ");
+           }
+           for (hash =0; hash < (actual*2)-1; hash++)
+           {
+               Console.Write("#");
+           }
+           Console.WriteLine();
+       }
+  
 
-        }
-        //Todo el código va aquí
-        static void dibujarPiramide(int niveles){
-            
-            
-            
+     Console.ReadKey();
+    
+     
+
+
+
+
+
         }
     }
 }
