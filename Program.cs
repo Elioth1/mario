2ace9cc336b43920ee7fde90ab95316e5650ccf2
Elioth1 <31458423+Elioth1@users.noreply.github.com>
2017-09-11 21:16:21 -0500

    Add files via upload
    Ahí está, usted me ayudó a hacerlo

--- Program.cs
+++ Program.cs
@@ -1,36 +1,49 @@
 ﻿using System;
-namespace programa
+
+namespace mario
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-       int altura, actual, esp, hash;
+            Console.WriteLine("Ingresa la altura de la piramide no mayor de 23 ni un numero que sea negativo");
+            Console.Write("Altura de la piramide: ");
+            int niveles;
+            /// Tryparse convierte la representación de una cadena (string) a su equivalente entero (int). Un valor devuelto indica si la conversión tuvo éxito.
+            //  devuelve 0 si no se pudo convertir
+            int.TryParse( Console.ReadLine(), out niveles);
 
-       Console.WriteLine("programadores: Elioth Marrujo de la Vega y Alejandra Lizette Gómez Tamayo");
-       Console.WriteLine("dibujar piramide");
-       Console.WriteLine("ingresa la altura de la piramide no mayor de 23 ni numero negativo");
-       altura = Convert.ToInt32(Console.ReadLine());
-       while( altura > 23 || altura < 1)
+             dibujarPiramide(niveles);
+
+        }
+        //Todo el código va aquí
+        static void dibujarPiramide(int niveles)
+        {
+            
+        
+       
+
+       
+      
+       
+      
+       while( niveles > 23 || niveles < 1)
        {
-           Console.Write("Altura: ");
-           int.TryParse(Console.ReadLine(),out altura);
+           Console.Write("Altura no compatible con el límite, ingrese de nuevo: ");
+           int.TryParse(Console.ReadLine(),out niveles);
        }
+       for (int i = 1; i <= niveles; i++)  //Aquí me ayudó usted xD
+       {                                   //No sé si jala porque me aparece System.FormatException sabe que y no sé moverle :b
+           var esc = new String('#',i);
+           System.Console.Write(esc.PadLeft(niveles));
+           System.Console.Write(" ");
+           System.Console.WriteLine(esc);
+       }    
+
      
-   for (esp = 0; esp < altura - actual; esp++)
-       {
-            for (hash =0; hash < (actual*2)-1; hash++)
-           {
-           Console.Write(" ");
-           }
-           for (hash =0; hash < (actual*2)-1; hash++)
-           {
-               Console.Write("#");
-           }
-           Console.WriteLine();
-       }
+   
   
-
+     Console.WriteLine("Lizette Alejandra Gómez Tamayo y Eioth Marrujo de la Vega");
      Console.ReadKey();
     
      
@@ -39,6 +52,9 @@ namespace programa
 
 
 
+            
+            
+            
         }
     }
 }
